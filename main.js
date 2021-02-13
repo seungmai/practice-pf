ac3eca588aedadc14bb48ee88aca01f64da2d0be
seungmai <76721552+seungmai@users.noreply.github.com>
2021-02-13 21:32:11 +0900

    Seungmin: portfolio(3)

--- main.js
+++ main.js
@@ -3,11 +3,16 @@
 const navbar = document.querySelector('#navbar');
 navbar.addEventListener = ('scroll', (e) => {
     const target = e.target.link();
-    console.log('#navbar');
 })
 
 $(document).on('scroll', function() {
     var navbar = $(document).scrollTOP();
     var navbarHeight = navbar + $(window).height();
     var tags = $('.tag');
+})
+
+const work = document.querySelector('#work');
+work.addEventListener = ('scroll', (e) => {
+    const a = e.target.link();
+    console.log('#work');
 })
