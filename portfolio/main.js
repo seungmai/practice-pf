78451f061fe7418b66261f8911570dcaa86d9779
seungmai <76721552+seungmai@users.noreply.github.com>
2021-02-15 16:20:49 +0900

    Seungmin: portfolio(6)

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,7 +1,33 @@
 'use strict'
 
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
+const navbar = document.querySelector('#navbar'); 
+const navbarHeight = navbar.getBoundingClientRect().height; 
+document.addEventListener('scroll', () => { 
+    console.log(`navbarHeight: ${navbarHeight}`); 
+    if (window.scrollY > navbarHeight) { 
+        navbar.classList.add('navbar--dark'); 
+    } else {
+        navbar.classList.remove('navbar--dark'); 
+    }
+});
+
+const navbarMenu = document.querySelector('.navbar__menu'); 
+navbarMenu.addEventListener('click', (event) => { 
+    const target = event.target; 
+    const link = target.dataset.link;
+    if (link === null) {
+        return; 
+    }
+    navbarMenu.classList.remove('open');
+    scrollIntoView(link); 
+    selectNavItem(target);
+});
+
+const navbar = document.querySelector('.navbar');
+navbar.addEventListener('click', () =>{
+    const target = event.target;
+    const link = target.dataset.link;
+})
 
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height; 
