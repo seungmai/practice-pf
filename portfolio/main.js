038dbbf4f2a770863e4e661d4a524cd34f3b4454
seungmai <76721552+seungmai@users.noreply.github.com>
2021-02-17 21:15:47 +0900

    Seungmin: portfolio(9)

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,37 +1,53 @@
 'use strict'
 
-const navbar = document.querySelector('#navbar'); 
-const navbarHeight = navbar.getBoundingClientRect().height; 
-document.addEventListener('scroll', () => { 
-    console.log(`navbarHeight: ${navbarHeight}`); 
-    if (window.scrollY > navbarHeight) { 
-        navbar.classList.add('navbar--dark'); 
+// Make navbar transparent when it is on the top(navbar를 투명하게 만들건데 올라가면 투명 내려가면 색깔있는 걸로!)
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log(`navbarHeight: ${navbarHeight}`);
+    if(window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
     } else {
-        navbar.classList.remove('navbar--dark'); 
+        navbar.classList.remove('navbar--dark');
     }
 });
 
-const navbarMenu = document.querySelector('.navbar__menu'); 
-navbarMenu.addEventListener('click', (event) => { 
-    const target = event.target; 
-    const link = target.dataset.link;
-    if (link === null) {
-        return; 
-    }
-    navbarMenu.classList.remove('open');
-    scrollIntoView(link); 
-    selectNavItem(target);
-});
-
+// Make home slowly fade to transparant as the window scrolls down(home을 scroll하면 투명하게!)
 const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height; 
-document.addEventListener('scroll', () => { 
-    home.style.opacity = 1 - window.scrollY / homeHeight; 
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
-const work = document.querySelector('.work__container');
-work.addEventListener('click', () => {
-    console.log('.work');
-    const target = target.link;
-    const work = target.event;
-}
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelector('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+    const active = document.querySelector('.categoty__btn selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
+    console.log(filter);
+    projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if(filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+        } else {
+            project.classList.add('invisible');
+        }
+    });
+});
+
+// 우리기 정의한 유틸리티 함수!
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+};
